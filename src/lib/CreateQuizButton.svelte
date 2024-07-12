3d5dd26d0d533de3bbb592b24e3823d2194b72e6
TheAnushervon <anushervon4j@gmail.com>
2024-07-12 14:08:41 +0300

    navigation to easy-questions complete

--- src/lib/CreateQuizButton.svelte
+++ src/lib/CreateQuizButton.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+	
+	import { Router, Link, Route } from "svelte-routing";
+	import {goto} from '$app/navigation';
 	function createNewQuiz(): void {}
+	function PassNewQuiz(): void {
+		goto('/easy-quiz'); 
+	}
 </script>
 
 <button class="create-quiz-button" on:click={createNewQuiz}> Create new quiz </button>
+<button class="pass-new-quiz" on:click={PassNewQuiz}>Pass-new-quiz</button>
 
 <style>
-	.create-quiz-button {
+	.create-quiz-button, .pass-new-quiz {
 		font-family: Kanit, sans-serif;
 		border-radius: 20px;
 		background-color: rgba(83, 232, 36, 0.53);
