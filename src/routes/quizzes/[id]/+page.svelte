30bcf19d13b0f5c40ae1feea42205d66c54f1596
muhammaduss <muhammad20040130@gmail.com>
2024-07-21 19:18:36 +0300

    fixing bugs

--- src/routes/quizzes/[id]/+page.svelte
+++ src/routes/quizzes/[id]/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import CheckLogin from '$lib/CheckLogin.svelte';
 	import QuizCreation from '$lib/QuizCreation.svelte';
 	export let data;
 	console.log(data);
 </script>
 
-<CheckLogin>
-	<main>
-		<QuizCreation {data} />
-	</main>
-</CheckLogin>
+<main>
+	<QuizCreation {data} />
+</main>
--- src/routes/take-quiz/[id]/+page.svelte
+++ src/routes/take-quiz/[id]/+page.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-	import CheckLogin from '$lib/CheckLogin.svelte';
 	import TakeQuiz from '$lib/TakeQuiz.svelte';
-	import QuizHeader from '$lib/QuizHeader.svelte';
 	export let data;
 	console.log(data);
 </script>
 
-<CheckLogin>
-	<main>
-		<TakeQuiz {data} />
-	</main>
-</CheckLogin>
+<main>
+	<TakeQuiz {data} />
+</main>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
